1b6bbd4e7a8f623885bca374d781ac530c981eaa
LucaUrti <lucaurti@gmail.com>
2020-04-29 12:24:24 +0200

    keep alive

---
+++ Broker.Common/WebAPI/Binance/Trade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Broker.Common.WebAPI.Binance
+{
+    public class Trade
+    {
+        public int id { get; set; }
+        public string price { get; set; }
+        public string qty { get; set; }
+        public string quoteQty { get; set; }
+        public long time { get; set; }
+        public bool isBuyerMaker { get; set; }
+        public bool isBestMatch { get; set; }
+    }
+}
---
+++ Broker.Common/WebAPI/Models/MyTrade.cs
@@ -0,0 +1,15 @@
+﻿using static Broker.Common.Strategies.Enumerator;
+
+namespace Broker.Common.WebAPI.Models
+{
+    public class MyTrade
+    {
+        public int id { get; set; }
+        public string price { get; set; }
+        public string qty { get; set; }
+        public string quoteQty { get; set; }
+        public long time { get; set; }
+        public TradeAction action { get; set; }
+        public bool isBestMatch { get; set; }
+    }
+}
